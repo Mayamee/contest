9e8650afea9aa65ccb6df09e248f634b384c6319
Mayamee <ponyashcat228@gmail.com>
2023-05-20 11:40:29 +0300

    cond done

--- src/conditioner/index.js
+++ src/conditioner/index.js
@@ -20,9 +20,24 @@ rl.on("close", () => {
   console.log(solveConditioner(tRoom, tCond, mode));
 });
 function solveConditioner(tRoom, tCond, mode) {
-  return {
-    tRoom,
-    tCond,
-    mode,
-  };
+  const avaliableModes = ["freeze", "heat", "auto", "fan"];
+  if (!avaliableModes.includes(mode)) return null;
+  if (tRoom === tCond) return tRoom;
+  if (mode === "auto") return tCond;
+  if (mode === "fan") return tRoom;
+  if (tRoom > tCond) {
+    switch (mode) {
+      case "freeze":
+        return tCond;
+      case "heat":
+        return tRoom;
+    }
+  } else {
+    switch (mode) {
+      case "freeze":
+        return tRoom;
+      case "heat":
+        return tCond;
+    }
+  }
 }
